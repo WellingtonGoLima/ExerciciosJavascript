50f25e0aaa8b6f12b0d36cc96fab101469fda4a7
Wellington Gonçalves de Lima <wellingtonecon5@gmail.com>
2025-08-14 23:36:47 -0300

    Aula introdutoria de OO

---
+++ oo.js
@@ -0,0 +1,19 @@
+// Procedural
+
+processamento(valor1, valor2, valor3)
+
+//00
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+
+    }
+
+}
+
+objeto.processamento({
+    
+})
